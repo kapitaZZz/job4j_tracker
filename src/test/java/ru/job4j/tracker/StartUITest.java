f17c3e62bd25f6b5319df6f552b61a729f5080cd
KapitaZzz <agarussky@gmail.com>
2022-03-26 13:36:25 +0300

    4.1. Разрыв зависимости StartUI от Scanner. [#181778 #280164]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -4,8 +4,9 @@ import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertTrue;
 
-public class StartUITest{
+public class StartUITest {
 
     @Test
     public void whenAddItem() {
@@ -29,4 +30,28 @@ public class StartUITest{
         assertThat(created.getName(), is(expected.getName()));
     }
 
+    @Test
+    public void whenEditItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        tracker.add(item);
+        String[] answers = {
+                String.valueOf(item.getId()),
+                "edited item"
+        };
+        StartUI.editItem(new StubInput(answers), tracker);
+        Item edited = tracker.findById(item.getId());
+        assertThat(edited.getName(), is("edited item"));
+    }
+
+    @Test
+    public void testDeleteItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("Some task");
+        tracker.add(item);
+        String[] answer = {String.valueOf(item.getId()), "another item"};
+        StartUI.deleteItem(new StubInput(answer), tracker);
+        Item expected = tracker.delete(item.getId());
+
+    }
 }
