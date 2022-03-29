3f506633966f7b6c5d67b47463effb3b7054eb39
KapitaZzz <agarussky@gmail.com>
2022-03-29 18:47:13 +0300

    2.1. Тест на ValidateInput [#311887 #281734]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -8,7 +8,7 @@ public class Item {
     private String name;
     private LocalDateTime created = LocalDateTime.now();
 
-//    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
 
     public Item() {
     }
